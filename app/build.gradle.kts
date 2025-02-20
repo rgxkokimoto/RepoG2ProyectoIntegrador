e7a90929a5780a8f80a267906df695cde333bae1
VAM51100 <vamvlwfmpmf@gmail.com>
2025-02-20 19:20:51 +0100

    Listado de experiencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
+    implementation(libs.play.services.base)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ app/src/main/java/com/example/g2int101experience/ListadoDeExperienciasAdapter.java
@@ -0,0 +1,36 @@
+package com.example.g2int101experience;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class ListadoDeExperienciasAdapter extends RecyclerView.Adapter<ListadoDeExperienciasViewHolder> {
+    private List<String> lista;
+
+    public ListadoDeExperienciasAdapter(List<String> lista) {
+        this.lista = lista;
+    }
+
+    @NotNull
+    @Override
+    public ListadoDeExperienciasViewHolder onCreateViewHolder(@NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item_layout, parent, false);
+        return new ListadoDeExperienciasViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NotNull ListadoDeExperienciasViewHolder holder, int position) {
+        holder.textView.setText(lista.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return lista.size();
+    }
+}
